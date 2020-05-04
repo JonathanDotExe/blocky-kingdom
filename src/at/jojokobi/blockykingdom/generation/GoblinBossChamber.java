7e289a3b8f3c1e7a86030c216e97dc245275cd7c
DatDonathan <jojo08082003@gmail.com>
2020-05-04 20:26:02 +0200

    Fixed iron bars in GoblinBossChamber

--- src/at/jojokobi/blockykingdom/generation/GoblinBossChamber.java
+++ src/at/jojokobi/blockykingdom/generation/GoblinBossChamber.java
@@ -42,7 +42,7 @@ public class GoblinBossChamber extends Structure {
 						material = Material.FIRE;
 					}
 					//Door
-					else if ((y > 0 && y <= 3) && (((x == 0 || x == getWidth() - 1) && (z >= getLength() / 2 - 1 && z <= getLength() / 2 + 2)) || ((z == 0 || z == getLength() - 1) && (x >= getWidth() / 2 - 1 || x <= getWidth() / 2 + 2)))) {
+					else if ((y > 0 && y <= 3) && (((x == 0 || x == getWidth() - 1) && (z >= getLength() / 2 - 1 && z <= getLength() / 2 + 2)) || ((z == 0 || z == getLength() - 1) && (x >= getWidth() / 2 - 1 && x <= getWidth() / 2 + 2)))) {
 						material = Material.IRON_BARS;
 					}
 					//Wall
