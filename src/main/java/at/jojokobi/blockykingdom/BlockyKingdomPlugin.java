359556e29c444fcaeaed2453303faf3cf2856b41
DatDonathan <jojo08082003@gmail.com>
2022-03-30 14:43:03 +0200

    Adjust spawn rates

--- src/main/java/at/jojokobi/blockykingdom/BlockyKingdomPlugin.java
+++ src/main/java/at/jojokobi/blockykingdom/BlockyKingdomPlugin.java
@@ -371,9 +371,9 @@ public class BlockyKingdomPlugin extends JavaPlugin implements Listener{
 
 						
 		//Spawns
-		spawner.addSpawn(new CustomEntitySpawnData(CustomSpawnsHandler.getInstance().getItem(DeathAngel.DEATH_ANGEL_KEY), 0.4, 3).setCanSpawn(p -> HeavenDimension.getInstance().isDimension(p.getWorld())));
-		spawner.addSpawn(new CustomEntitySpawnData(CustomSpawnsHandler.getInstance().getItem(Goblin.GOBLIN_SPAWN_KEY), 0.2, 2).setCanSpawn(p -> p.getWorld().getTime() >= 12300 && p.getWorld().getTime() < 23850));
-		spawner.addSpawn(new CustomEntitySpawnData(CustomSpawnsHandler.getInstance().getItem(EliteGoblin.ELITE_GOBLIN_SPAWN_KEY), 0.05, 1).setCanSpawn(p -> p.getWorld().getTime() >= 12300 && p.getWorld().getTime() < 23850));
+		spawner.addSpawn(new CustomEntitySpawnData(CustomSpawnsHandler.getInstance().getItem(DeathAngel.DEATH_ANGEL_KEY), 0.4, 3).setCanSpawn(p -> HeavenDimension.getInstance().isDimension(p.getWorld())).setMaxEntitiesAround(32));
+		spawner.addSpawn(new CustomEntitySpawnData(CustomSpawnsHandler.getInstance().getItem(Goblin.GOBLIN_SPAWN_KEY), 0.4, 4).setCanSpawn(p -> p.getWorld().getTime() >= 12300 && p.getWorld().getTime() < 23850).setMaxEntitiesAround(16));
+		spawner.addSpawn(new CustomEntitySpawnData(CustomSpawnsHandler.getInstance().getItem(EliteGoblin.ELITE_GOBLIN_SPAWN_KEY), 0.05, 1).setCanSpawn(p -> p.getWorld().getTime() >= 12300 && p.getWorld().getTime() < 23850).setMaxEntitiesAround(16));
 
 		
 		//Happiness Handler
