53d9811b93c42385191c9e3e6c7a7c8b6608c07f
DatDonathan <jojo08082003@gmail.com>
2022-06-16 17:15:15 +0200

    Removing bottom of heaven using populator

--- src/main/java/at/jojokobi/blockykingdom/dimensions/HeavenGenerator.java
+++ src/main/java/at/jojokobi/blockykingdom/dimensions/HeavenGenerator.java
@@ -1,9 +1,14 @@
 package at.jojokobi.blockykingdom.dimensions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.LimitedRegion;
 import org.bukkit.generator.WorldInfo;
 import org.bukkit.util.noise.NoiseGenerator;
 import org.bukkit.util.noise.SimplexNoiseGenerator;
@@ -29,7 +34,6 @@ public class HeavenGenerator extends ChunkGenerator {
 				for (int yPos = 0; yPos < height; yPos++) {
 					data.setBlock(xPos, yPos, zPos, Material.AIR);
 				}
-				data.set
 			}
 		}
 		//Clouds
@@ -46,6 +50,44 @@ public class HeavenGenerator extends ChunkGenerator {
 			}
 		}
 		System.out.println("Surface " + x + "/" + z);
+	}
+	
+	@Override
+	public List<BlockPopulator> getDefaultPopulators(World world) {
+		List<BlockPopulator> populators = new ArrayList<BlockPopulator>(super.getDefaultPopulators(world));
+		populators.add(new BlockPopulator() {
+			@Override
+			public void populate(WorldInfo worldInfo, Random random, int x, int z, LimitedRegion limitedRegion) {
+				super.populate(worldInfo, random, x, z, limitedRegion);
+				//Remove bottom
+				NoiseGenerator generator = new MultiNoiseGenerator(world.getSeed(), 5);
+				for (int xPos = 0; xPos < TerrainGenUtil.CHUNK_WIDTH; xPos++) {
+					for (int zPos = 0; zPos < TerrainGenUtil.CHUNK_LENGTH; zPos++) {
+						int height = (int) generator.noise((x * TerrainGenUtil.CHUNK_WIDTH + xPos) * 0.005, (z * TerrainGenUtil.CHUNK_LENGTH + zPos) * 0.005) * 50 + 50;
+						boolean ended = false;
+						for (int yPos = 0; yPos < height && ended; yPos++) {
+							Material type = limitedRegion.getType(x * TerrainGenUtil.CHUNK_WIDTH + xPos, yPos, z * TerrainGenUtil.CHUNK_LENGTH + zPos);
+							ended = !type.isSolid() || type.isAir();
+							limitedRegion.setType(x * TerrainGenUtil.CHUNK_WIDTH + xPos, yPos, z * TerrainGenUtil.CHUNK_LENGTH + zPos, Material.AIR);
+						}
+					}
+				}
+				//Clouds
+				for (int y = 30; y < 55; y += 5) {
+					NoiseGenerator g = new SimplexNoiseGenerator(world.getSeed() + y);
+					for (int i = 0; i < TerrainGenUtil.CHUNK_WIDTH; i++) {
+						for (int j = 0; j < TerrainGenUtil.CHUNK_LENGTH; j++) {
+							if (g.noise((x * TerrainGenUtil.CHUNK_WIDTH + i) * 0.025, (z * TerrainGenUtil.CHUNK_LENGTH + j) * 0.025) > 0.7) {
+								limitedRegion.setType(x * TerrainGenUtil.CHUNK_WIDTH + x, y, z * TerrainGenUtil.CHUNK_LENGTH + j, Material.WHITE_WOOL);
+								limitedRegion.setType(x * TerrainGenUtil.CHUNK_WIDTH + x, y + 1, z * TerrainGenUtil.CHUNK_LENGTH + j, Material.WHITE_WOOL);
+								limitedRegion.setType(x * TerrainGenUtil.CHUNK_WIDTH + x, y + 2, z * TerrainGenUtil.CHUNK_LENGTH + j, Material.WHITE_WOOL);							
+							}
+						}
+					}
+				}
+			}
+		});
+		return populators;
 	}
 	
 	@Override
