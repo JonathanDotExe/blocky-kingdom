30837465166c675427024f37ca77daa3e7355fbc
DatDonathan <jojo08082003@gmail.com>
2022-06-16 14:35:31 +0200

    Moved bottom clearing in heaven

--- src/main/java/at/jojokobi/blockykingdom/dimensions/HeavenGenerator.java
+++ src/main/java/at/jojokobi/blockykingdom/dimensions/HeavenGenerator.java
@@ -16,27 +16,27 @@ public class HeavenGenerator extends ChunkGenerator {
 	@Override
 	public void generateNoise(WorldInfo world, Random random, int x, int z, ChunkData data) {
 		super.generateNoise(world, random, x, z, data);
-		//Remove bottom
-		NoiseGenerator generator = new MultiNoiseGenerator(world.getSeed(), 5);
-		for (int xPos = 0; xPos < TerrainGenUtil.CHUNK_WIDTH; xPos++) {
-			for (int zPos = 0; zPos < TerrainGenUtil.CHUNK_LENGTH; zPos++) {
-				int height = (int) generator.noise((x * TerrainGenUtil.CHUNK_WIDTH + xPos) * 0.005, (z * TerrainGenUtil.CHUNK_LENGTH + zPos) * 0.005) * 40 + 60;
-				for (int yPos = world.getMinHeight(); yPos < height; yPos++) {
-					data.setBlock(xPos, yPos, zPos, Material.AIR);
-				}
-			}
-		}
 	}
 	
 	@Override
 	public void generateCaves(WorldInfo world, Random random, int x, int z, ChunkData data) {
 		super.generateCaves(world, random, x, z, data);
+		//Remove bottom
+		NoiseGenerator generator = new MultiNoiseGenerator(world.getSeed(), 5);
+		for (int xPos = 0; xPos < TerrainGenUtil.CHUNK_WIDTH; xPos++) {
+			for (int zPos = 0; zPos < TerrainGenUtil.CHUNK_LENGTH; zPos++) {
+				int height = (int) generator.noise((x * TerrainGenUtil.CHUNK_WIDTH + xPos) * 0.005, (z * TerrainGenUtil.CHUNK_LENGTH + zPos) * 0.005) * 40 + 60;
+				for (int yPos = data.getMinHeight() + 1; yPos < height; yPos++) {
+					data.setBlock(xPos, yPos, zPos, Material.AIR);
+				}
+			}
+		}
 		//Clouds
 		for (int y = 30; y < 55; y += 5) {
-			NoiseGenerator generator = new SimplexNoiseGenerator(world.getSeed() + y);
+			NoiseGenerator g = new SimplexNoiseGenerator(world.getSeed() + y);
 			for (int i = 0; i < TerrainGenUtil.CHUNK_WIDTH; i++) {
 				for (int j = 0; j < TerrainGenUtil.CHUNK_LENGTH; j++) {
-					if (generator.noise((x * TerrainGenUtil.CHUNK_WIDTH + i) * 0.025, (z * TerrainGenUtil.CHUNK_LENGTH + j) * 0.025) > 0.7) {
+					if (g.noise((x * TerrainGenUtil.CHUNK_WIDTH + i) * 0.025, (z * TerrainGenUtil.CHUNK_LENGTH + j) * 0.025) > 0.7) {
 						data.setBlock(i, y, j, Material.WHITE_WOOL);
 						data.setBlock(i, y + 1, j, Material.WHITE_WOOL);
 						data.setBlock(i, y + 1, j, Material.WHITE_WOOL);
